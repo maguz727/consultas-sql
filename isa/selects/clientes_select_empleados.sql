371e9b221daff7d0de529a3a550bfc08870b58af
Mnauel Guzman <maguz727@gmail.com>
2024-09-12 13:05:34 -0500

    Se crea consulta clientes_select_empleados.sql

---
+++ isa/selects/clientes_select_empleados.sql
@@ -0,0 +1,28 @@
+SELECT
+    UPPER(tp.item) AS 'TIPO DE CLIENTE',
+    UPPER(c.classification) AS 'CLASIFICACIÓN',
+    UPPER(c.businessName) AS 'NOMBRE DEL CLIENTE',
+    c.documentNumber AS 'NIT',
+    CASE c.`status`
+        WHEN 1 THEN 'ACTIVO'
+        WHEN 0 THEN 'INACTIVO'
+        WHEN -1 THEN 'RETIRADO'
+        ELSE ''
+    END AS 'ESTADO DEL CLIENTE',
+    e.fullName AS 'NOMBRE DEL EMPLEADO',
+    CASE ce.isActive
+        WHEN 1 THEN 'ACTIVO'
+        WHEN 0 THEN 'INACTIVO'
+        ELSE ''
+    END AS 'ESTADO DEL EMPLEADO'
+FROM wg_customers AS c
+    JOIN wg_customer_employee AS ce 
+        ON c.id = ce.customer_id
+    lEFT JOIN wg_employee AS e 
+        ON ce.employee_id = e.id
+    LEFT JOIN (
+        SELECT sp.item, sp.`value`
+        FROM system_parameters AS sp
+        WHERE sp.`group` = 'tipocliente'
+    ) tp ON tp.`value` COLLATE utf8_general_ci = c.type
+ORDER BY e.`fullName` ASC
